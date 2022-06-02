4bbaa4986b62dde646cc6b5600d32fce41fe0658
kadank <kadank3@gmail.com>
2022-06-02 17:40:55 +0200

    Added basic gameplay features
    basic mechanics like roll and shoot

---
+++ Roulette/Roulette/Roulette.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <time.h> 
+#include <conio.h>
+
+using namespace std;
+
+char choice;
+
+int chamber = rand() % 6 + 1;
+
+void ded() {
+    cout << "you are dead lol" << endl;
+}
+
+bool trigger() {        //shoots
+
+    if (chamber == 1)
+    {
+        ded();
+        chamber = rand() % 6 + 1; //if got shot, roll the chamber and pass
+        return true;
+    }
+    else
+    {
+        chamber--;
+        return false;             //if alive, go down by one and pass
+    }   
+
+}
+
+void roll() {           //rolls the chamber and shoots
+
+    chamber = rand() % 6 + 1;
+    trigger();
+
+}
+
+void decision() {
+    bool err = false;
+    do
+    {
+        cout << chamber << endl;
+        choice = _getch();
+
+        cout << choice << endl;
+
+        err = false;
+        switch (choice)
+        {
+        case 'r':
+            roll();
+            break;
+        case 's':
+            trigger();
+            break;
+        default:
+            cout << "wrong input" << endl;
+            err = true;
+        }
+    } while (err == true);
+}
+
+
+int main()
+{
+    srand(time(NULL));
+    
+    
+
+    for (;;) {
+        
+        decision();
+       
+
+
+
+    }
+
+
+    
+}
+
+
+
+
+
